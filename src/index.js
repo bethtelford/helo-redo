a817559427052d701d3e0f05a81f2f8925d20906
Kedirech <brackcarmony@gmail.com>
2018-04-17 14:58:54 -0600

    Update index

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import App from './App';
 import { unregister } from './registerServiceWorker';
 
 import './reset.css';
+import axios from 'axios';
+
+// Checking if there is a BASEURL that is being set for this project.
+// If so use it
+if (process.env.REACT_APP_BASEURL){
+    axios.defaults.baseUrl = process.env.REACT_APP_BASEURL;
+}
+
 
 ReactDOM.render(
   <Provider store={store}>
