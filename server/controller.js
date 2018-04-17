a40a1067b3bba1d61cb177a928a09d5b6130321e
Kedirech <brackcarmony@gmail.com>
2018-04-17 16:04:56 -0600

    fix db for new peeps.

--- server/controller.js
+++ server/controller.js
@@ -52,7 +52,7 @@ module.exports = {
             req.session.userid = user[0].id;
             res.status(200).send(user[0]);
           } else {
-            db.user.create_user(username, password, `https://robohash.org/${username}.png`)
+            req.app.get('db').user.create_user(username, password, `https://robohash.org/${username}.png`)
               .then(user => {
                 req.session.userid = user[0].id;
                 res.status(200).send(user[0]);
